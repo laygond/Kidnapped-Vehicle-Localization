2700c758ae8aea193cf942378972c978c67b8bd4
laygond <laygond_bryan@hotmail.com>
2020-07-07 18:35:09 -0400

    :sparkles: deletes previous build automatically

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Script to build all components from scratch, using the maximum available CPU power
+# Script to delete old build folder and build it from scratch
 #
 # Given parameters are passed over to CMake.
 # Examples:
 #    * ./build.sh -DCMAKE_BUILD_TYPE=Debug
 #    * ./build.sh VERBOSE=1
 #
+# Written by Bryan Laygond
 
 cd `dirname $0` # where this bash script is contained.
 
